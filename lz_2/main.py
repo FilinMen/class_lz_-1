0c62b55935299aeba899c6fe7fdcafe0ef50ca62
FilinMen <fiilinkadabra@tutamail.com>
2025-08-27 10:43:30 +0300

    end

---
+++ lz_2/main.py
@@ -0,0 +1,9 @@
+import inherit as inh
+
+
+def main():
+    
+    inh.main()
+
+if __name__ == "__main__":
+    main()
---
+++ lz_3/main.py
@@ -0,0 +1,8 @@
+import capsul as cap
+
+def main():
+    
+    cap.main()
+
+if __name__ == "__main__":
+    main()
